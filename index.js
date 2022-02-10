533aec22b3bb1f6fffb8cc03516a305e9ca2344b
C0mbed <matt@farncombe.co>
2022-02-10 09:29:49 -0700

    commit tree 9fbf25c353d889a6aa389a40d586e2d6cf521b40

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(element => {
+    let title = element.split(" ").map( word => {
+      return (word[0].toUpperCase() + word.slice(1))
+    })
+    return title.join(" ")
+  })
 }
